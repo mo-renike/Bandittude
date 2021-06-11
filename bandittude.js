69e784c963c78a07166721aebf100192a56a5499
Morenike Oyewole <herroyalpianist@gmail.com>
2021-06-11 15:17:40 +0100

    base files

---
+++ bandittude.js
@@ -0,0 +1,39 @@
+
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -100px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries,appearOnScroll){
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  })
+  
+},appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+})
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+  // console.log(slider)
+})
+
+
+ function show(){
+alert ("Thank you for submitting. Your message has been recieved and a reply would be sent shortly.");
+}
+
+
+$('.carousel').carousel({
+  interval: 2000
+})
